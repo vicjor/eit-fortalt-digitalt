5661d4bc62de8f58a6c3f4513b8e4d41622e8561
Victor Jørgensen <victor.jorgens@gmail.com>
2021-02-10 13:02:01 +0100

    Digital Art init. Fetch temp data from Adressaparken

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,52 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+export default function Home({ data, date }) {
+	const [tempData, setData] = useState(data);
+	const [temp, setTemp] = useState([]);
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+	useEffect(() => {
+		renderTemperature();
+	}, []);
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+	async function renderTemperature() {
+		if (tempData) {
+			document.body.style = "color: blue";
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+			setTemp(
+				<ul>
+					{data.map((x) => (
+						<li key={x.date} style={{ margin: "1em" }}>
+							<strong>🕰 {String(x.date).slice(11, 16)} </strong>
+							{Math.round(x.value)}º
+						</li>
+					))}
+				</ul>
+			);
+		}
+	}
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+	return (
+		<div style={{ margin: "1em" }}>
+			<h1>Temperaturmålinger i Adressaparken</h1>
+			<h2>{date ?? date}</h2>
+			<h3>Klokkeslett | Temparatur</h3>
+			{temp && temp}
+		</div>
+	);
+}
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+export async function getServerSideProps(context) {
+	let response = await fetch(
+		"https://api.adressaparken.no/v1/sensorDataList?limit=100&offset=0&sensor=TC"
+	);
+	let temp = await response.json();
+	let date = new Date().toDateString();
+	return {
+		props: {
+			data: temp,
+			date: date,
+		},
+	};
 }

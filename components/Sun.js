26d1b95293d74b7945d833e3a3faa52f482f7150
Victor Jørgensen <victor.jorgens@gmail.com>
2021-04-06 20:40:58 +0200

    Fix sun pos

--- components/Sun.js
+++ components/Sun.js
@@ -4,7 +4,7 @@ import styles from "./Sun.module.scss";
 // Inspired by https://css-irl.info/heatwave-animated-sun-illustration/
 export default function Sun(props) {
 	return (
-		<Draggable>
+		<Draggable defaultPosition={{ x: 300, y: -300 }}>
 			<div className={styles.sun} style={{ opacity: "50%" }}></div>
 		</Draggable>
 	);
